3ef3bc29954f215637da9d05206ea8315ecba4e5
Fabian Hobi <fabian.hobi@bluewin.ch>
2019-07-27 22:48:41 +0200

    Formating and preparation of game.js

--- game.js
+++ game.js
@@ -1,9 +1,13 @@
 // canvas
-
 var canvas = document.getElementById("gameCanvas");
 var ctx = canvas.getContext("2d");
 let width = canvas.width;
 let height = canvas.height;
 
+// draw
 
 
+// update
+
+
+// game loop
